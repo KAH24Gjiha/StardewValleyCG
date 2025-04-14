2abe507e84ed21b0941caabfad5f6fd8201554aa
KAH24Gjiha <128780283+KAH24Gjiha@users.noreply.github.com>
2025-04-14 10:15:07 +0900

    Sprite-And-Move
    스프라이트 추가 및 기본적인 플레이어 이동 구현 (오로지 이동만, 상호작용은 새 컴포넌트 추가 요망)

---
+++ StardewValleyCG/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private int playerSpeed;
+
+    private Rigidbody2D rigid;
+
+    void Start()
+    {
+        rigid = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        rigid.velocity = new Vector2(horizontal, vertical) * playerSpeed;
+
+        //Shift ½Ã ´À·ÁÁü
+        if (Input.GetKey(KeyCode.LeftShift)) { rigid.velocity /= 2; }
+
+        Camera.main.transform.position = this.transform.position - Vector3.forward;
+
+    }
+}
